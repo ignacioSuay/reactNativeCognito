30a82f9cef96d451c0eecb46d971d2ec1aa89092
44087895 <ignacio.s.mas@noexternalmail.hsbc.com>
2018-04-27 10:18:49 +0100

    First part of the login page

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.username = "";
+        this.password = "";
+    }
+
+    _handleTextChange = event => {
+        this.username = event.nativeEvent.text;
+        console.log("the username is " + this.username);
+    };
+
+    _loginOnPress = event => {
+        console.log("button pressed")
+    };
+
+    render() {
+        console.log("rerender");
+        return (
+            <View style={styles.container}>
+                <Text>Username:</Text>
+                <TextInput style={styles.input} onSubmitEditing={this._handleTextChange}/>
+                <Button title="Login" onPress={this._loginOnPress}/>
+            </View>
+        );
+    }
 }
 
+const sum = function (a, b) {
+    return a + b;
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        fontSize: 20,
+        borderWidth: 2,
+        height: 40,
+        width: 250
+    }
 });
